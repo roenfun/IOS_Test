a2c0b94cb4198b11aac9c73c82575d3a96a77e35
roenfun <roenfun@163.com>
2016-02-22 10:56:17 +0800

    add some log message

--- IOS_Automation_Study/Instruments_Study/Test.js
+++ IOS_Automation_Study/Instruments_Study/Test.js
@@ -20,8 +20,8 @@ if(app.windows()[0].tableViews()[1].buttons()["退出登录"].isVisible()){
     // Alert detected. Expressions for handling alerts should be moved into the UIATarget.onAlert function definition.
     UIATarget.onAlert = function onAlert(alert) {
         var title = alert.name;
-        UIALogger.logMessage("Alert with title '" + title + "' encountered.");
-        UIALogger.logDebug( "confirm to logout..." );
+        UIALogger.logMessage("Debug: Alert with title '" + title + "' encountered.");
+        UIALogger.logDebug( "Debug: confirm to logout..." );
         target.frontMostApp().alert().buttons()["确定"].tap();
         
         return true; //处理警告,返回true,跳过默认的处理方法
@@ -36,7 +36,7 @@ if(app.windows()[0].tableViews()[1].buttons()["退出登录"].isVisible()){
     target.delay(1);
     app.windows()[0].navigationBar().buttons()["topbar back normal"].tap();
 }else{
-    UIALogger.logMessage("User has been Logout!");
+    UIALogger.logMessage("Debug: User has been Logout...");
     //------ Back To Homepage---------
     target.delay(1);
     app.windows()[0].navigationBar().buttons()["topbar back normal"].tap();
@@ -47,16 +47,26 @@ if(app.windows()[0].tableViews()[1].buttons()["退出登录"].isVisible()){
 //------ Logout End----------
 
 //scroll to bottom
-for(i=0;i<200;i++){
+UIALogger.logDebug( "Debug: scroll to bottom..." );
+for(i=0;i<180;i++){
+    UIALogger.logDebug( "<Debug>: scroll count: " + i );
     target.dragInsideWithOptions({startOffset:{x:0.5, y:0.8}, endOffset:{x:0.5, y:0.2}, duration:1.5});
     target.delay(1);
 }
 
-target.dragInsideWithOptions({touchCount:2, startOffset:{x:0.5, y:0.8}, endOffset:{x:0.5, y:0.8}, duration:2.5});
-target.delay(1);
+//scroll to top
+UIALogger.logDebug( "Debug: scroll to top..." );
+target.touchAndHold({x:344, y:642}, 3);
+target.delay(2);
+app.windows()[0].staticTexts()[3].tap();
+//target.tap({x:344, y:642});//屏幕(x,y) 坐标单击
 
+target.delay(5);
+
+//screen capture
+target.dragInsideWithOptions({touchCount:2, startOffset:{x:0.5, y:0.8}, endOffset:{x:0.5, y:0.8}, duration:2.5});
+target.delay(2);
 //app.logElementTree();
-//target.tapWithOptions({tapOffset:{x:0.50, y:0.60}});
 target.tap({x:187, y:480});//屏幕(x,y) 坐标单击
-target.delay(1);
+target.delay(2);
 target.tap({x:187, y:496});
